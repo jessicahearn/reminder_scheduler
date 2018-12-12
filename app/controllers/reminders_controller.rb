d235c593b54ee32d4decfcbe69accf611e0acde8
Jessica Hearn <jessica.hearn.09@gmail.com>
2018-12-12 21:52:15 +0100

    Add delete functionality for reminders.

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,12 +8,26 @@ class RemindersController < ApplicationController
   end
 
   def create
-    reminder = Reminder.new(user: current_user, title: params[:reminder][:title], description: params[:reminder][:description])
-    reminder.save!
-    redirect_to reminders_path
+    reminder = Reminder.new(user: current_user, 
+                            title: params[:reminder][:title], 
+                            description: params[:reminder][:description])
+    if reminder.save!
+      flash[:notice] = "New reminder created!"
+      redirect_to reminders_path
+    else
+      flash[:alert] = "Post cannot be saved. Please make sure you've included all necessary information."
+    end
   end
 
   def show
     @reminder = Reminder.where(id: params[:id]).first
+  end
+
+  def destroy
+    @reminder = Reminder.where(id: params[:id]).first
+    @reminder.destroy
+
+    flash[:notice] = "Reminder deleted"
+    redirect_to reminders_path
   end
 end
